6b80fe52885d4d4ce67676c432ac74e690317e1e
ropgocTb <216320907+ropgocTb@users.noreply.github.com>
2025-11-26 17:42:45 +0300

    Added: film and user models

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,31 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
+
+import java.time.Duration;
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
 public class Film {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    @JsonProperty("duration")
+    private Duration duration;
+
+    public long getDuration() {
+        return duration.toMinutes();
+    }
+
+    public void setDuration(long duration) {
+        this.duration = Duration.ofMinutes(duration);
+    }
 }
--- src/test/java/ru/yandex/practicum/filmorate/FilmsTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmsTests.java
@@ -7,7 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 class FilmorateApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+    public void EmptyFilmNameTest() {
 
+    }
+
+    @Test
+    public void 
 }
