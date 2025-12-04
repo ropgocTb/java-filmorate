75d11167218ab1da22f25d5d5045c111b4b93042
ropgocTb <216320907+ropgocTb@users.noreply.github.com>
2025-12-04 22:06:51 +0300

    Updated: model changes for db functioning

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,26 +1,33 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Data;
 
-import java.time.Duration;
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Data
+@Builder
 public class Film {
     private long id;
     private String name;
     private String description;
     private LocalDate releaseDate;
-    private Duration duration;
+    private long duration;
     private Set<Long> likes = new HashSet<>();
+    private Long likesCount;
+    private Rating mpa;
+    @JsonProperty("genres")
+    private List<Genre> genres;
 
-    public long getDuration() {
-        return duration.toMinutes();
-    }
-
-    public void setDuration(long duration) {
-        this.duration = Duration.ofMinutes(duration);
+    public Map<String, Object> toMap() {
+        Map<String, Object> values = new HashMap<>();
+        values.put("title", name);
+        values.put("description", description);
+        values.put("release_date", releaseDate);
+        values.put("duration", duration);
+        values.put("rating_id", mpa.getId());
+        return values;
     }
 }
